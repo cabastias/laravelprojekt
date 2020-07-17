c5403eed32f4cb2b45798f947135e433f016218f
cassandra <cassandra.bastias@hotmail.com>
2020-07-17 23:00:31 +0200

    Fixed links

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -5,16 +5,19 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Notes</div>
+                <div class="card-header">Dashboard</div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
+                    <h2>Notes</h2>
                     @foreach($notes as $note)
                         <a href="{{ url('notes/view', $note['slug']) }}">{{ $note['title'] }}</a><br>
                     @endforeach
+                    <hr>
+                    <a href="{{ URL::previous() }}" class="btn btn-success">Create new note</a>
                 </div>
             </div>
         </div>
